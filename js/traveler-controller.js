f5e83df84e7a33624d3f3f42840203bd65a75efa
AbirNadav <pakakaw41@gmail.com>
2020-04-19 15:43:29 +0300

    3

--- js/traveler-controller.js
+++ js/traveler-controller.js
@@ -1,1 +1,30 @@
 'use strict'
+
+function initMap() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 12,
+        center: { lat: -gCurrLat, lng: gCurrLng },
+        mapTypeControl: true,
+        mapTypeControlOptions: {
+            style: google.maps.MapTypeControlStyle.HORIZONTAL_BAR,
+            position: google.maps.ControlPosition.TOP_CENTER
+        },
+        zoomControl: true,
+        zoomControlOptions: {
+            position: google.maps.ControlPosition.LEFT_CENTER
+        },
+        scaleControl: true,
+        streetViewControl: true,
+        streetViewControlOptions: {
+            position: google.maps.ControlPosition.LEFT_TOP
+        },
+        fullscreenControl: true
+    });
+}
+
+
+
+var gtelaviv = {
+    lat: gCurrLat,
+    lng: gCurrLng
+};
--- js/traveler-service.js
+++ js/traveler-service.js
@@ -1,3 +1,11 @@
 'use strict'
-var gCurrLng
-var gCurrLat
+var gCurrLng = 34.7671218
+var gCurrLat = 32.080525
+
+function moveto(location) {
+
+
+    google.maps.event.addDomListener(outer, 'click', function() {
+        map.setCenter(location)
+    });
+}
